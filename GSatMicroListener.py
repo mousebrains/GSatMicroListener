701fb258c79a14171772dd9cfb0a684484552bbf
Pat Welch <pat@mousebrains.com>
2020-02-14 16:03:14 -0800

    Initial checkin

---
+++ GSatMicroListener.py
@@ -0,0 +1,129 @@
+#! /usr/bin/env python3
+#
+# Listen to a socket
+# When a connection is made,
+# spawn a thread, parse the message,
+# and send to the writer thread.
+#
+# Feb-2020, Pat Welch, pat@mousebrains.com
+
+import time
+import threading
+import socket
+import queue
+import argparse
+import logging
+import logging.handlers
+
+def addLoggerArgs(parser:argparse.ArgumentParser) -> None:
+    grp = parser.add_argument_group('Logger Related Options')
+    grp.add_argument('--logfile', type=str, help='Name of logfile')
+    grp.add_argument('--logBytes', type=int, default=10000000, help='Maximum logfile size in bytes')
+    grp.add_argument('--logCount', type=int, default=3, help='Number of backup files to keep')
+    grp.add_argument('--verbose', action='store_true', help='Enable verbose logging')
+
+def mkLogger(args:argparse.ArgumentParser) -> logging.Logger:
+    logger = logging.getLogger()
+    if args.logfile:
+        ch = logging.handlers.RotatingFileHandler(args.logfile,
+                maxBytes=args.logBytes,
+                backupCount=args.logCount)
+    else:
+        ch = logging.StreamHandler()
+
+    if args.verbose:
+        logger.setLevel(logging.DEBUG)
+        ch.setLevel(logging.DEBUG)
+    else:
+        logger.setLevel(logging.INFO)
+        ch.setLevel(logging.INFO)
+
+    formatter = logging.Formatter('%(asctime)s %(levelname)s: %(message)s')
+    ch.setFormatter(formatter)
+
+    logger.addHandler(ch)
+
+    return logger
+
+class Writer(threading.Thread):
+    ''' Wait on a queue, and write the item to a file '''
+    def __init__(self, fn:str, logger:logging.Logger):
+        threading.Thread.__init__(self, daemon=True)
+        self.name = 'Parser'
+        self.fn = fn
+        self.logger = logger
+        self.q = queue.Queue()
+    def run(self) -> None:
+        '''Called on thread start '''
+        while True: # Loop forever
+            try:
+                item = self.q.get()
+                self.q.task_done()
+                self.logger.info('Item=%s', item)
+                with open(self.fn, 'ab') as fp:
+                    fp.write(item)
+            except:
+                self.logger.exception('Exception while writing to %s', self.fn)
+
+class Parser(threading.Thread):
+    ''' Read from a connection, parse it, and send to the output queue '''
+    def __init__(self, conn, addr, logger:logging.Logger, q:queue.Queue):
+        threading.Thread.__init__(self, daemon=True)
+        self.name = 'Parser'
+        self.conn = conn
+        self.addr = addr
+        self.logger = logger
+        self.q = q
+
+    def __parser(self, msg:bytes) -> str:
+        ''' Parse msg and return a string to be written to a file '''
+        self.logger.info('MSG=%s', msg)
+        result = msg # Add real parser here
+        return result
+
+    def run(self) -> None:
+        '''Called on thread start '''
+        try:
+            msg = b''
+            with self.conn as conn:
+                while True: # Get the whole message until the socket is closed
+                    data = conn.recv(8192) # Get the data
+                    self.logger.debug('data=%s', data)
+                    if not data: break # connection has dropped
+                    msg += data
+            self.q.put(self.__parser(msg))
+        except:
+            self.logger.exception('Exception while reading from address %s', self.addr)
+
+parser = argparse.ArgumentParser(description="Listen for a GSatMicro message")
+addLoggerArgs(parser)
+parser.add_argument('--output', type=str, required=True, help='File to write to')
+grp = parser.add_argument_group('Listener Related Options')
+grp.add_argument('--port', type=int, required=True, help='Port to listen on')
+grp.add_argument('--maxConnections', type=int, default=10, 
+            help='Maximum number of simultaneous connections')
+args = parser.parse_args()
+
+logger = mkLogger(args)
+logger.info('args=%s', args)
+
+try:
+    writer = Writer(args.output, logger) # Create the writer thread
+    writer.start() # Start the writer thread
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        logger.debug('Opened socket')
+        s.bind(('', args.port))
+        logger.debug('Bound to port %s', args.port)
+        s.listen()
+        logger.debug('Listening to socket')
+        while writer.is_alive(): # If writer is alive then try for another connection
+            (conn, addr) = s.accept() # Wait for a connection
+            logger.info('Connection from %s', addr)
+            thrd = Parser(conn, addr, logger, writer.q) # Create a new parser thread
+            thrd.start() # Start the new parser thread
+            logger.info('n Threads %s', threading.active_count())
+            while threading.active_count() > args.maxConnections: # don't overload the system
+                time.sleep(1) # Wait for a thread to die before accepting anything else
+except:
+    logger.exception('Unexpected exception while listening')
